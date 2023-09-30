c23a2087928ea0c5adb3e95d6fe20c5e365a989d
Aaditya <aadityapuriaaditya@gmail.com>
2023-09-30 11:25:01 +0530

    variables and datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "any@gmail.com"
+var accountPass = "12345"
+accountCity = "Jaipur"
+
+accountEmail="hc@hc.com"
+accountPass="1212121212"
+accountCity="Bengaluru"
+
+console.table([accountId, accountEmail, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,17 @@
+"use strict" // treat all JS as newwer version
+
+let nme="aaditya"
+let age = 18
+let loggerdIn = false
+
+//number => 2^53
+//boolean
+//string
+//bigInt
+//null
+//undefined
+//symbol=>unique
+
+//object
+
+console.log(typeof null);
