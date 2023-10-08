bb73e44a9ef30c738723a054202fa4c8b28f4756
Aaditya <aadityapuriaaditya@gmail.com>
2023-10-08 23:12:53 +0530

    Project 2 completed

--- 06_Projects/Color Changer/index.js
+++ 06_Projects/Color Changer/index.js
@@ -1,20 +1,21 @@
 const buttons = document.querySelectorAll('.button');
+const style = document.body.style;
 buttons.forEach((button) => {
   // console.log(button);
   button.addEventListener('click',(e)=>{
     // console.log(e);
     // console.log(e.target);
     if(e.target.id === "white"){
-      document.body.style.backgroundColor = e.target.id;
+      style.backgroundColor = e.target.id;
     }
     else if(e.target.id === "grey"){
-      document.body.style.background=e.target.id;
+      style.background=e.target.id;
     }
     else if(e.target.id === "blue"){
-      document.body.style.background=e.target.id;
+      style.background=e.target.id;
     }
     else if(e.target.id === "yellow"){
-      document.body.style.background=e.target.id;
+      style.background=e.target.id;
     }
   });
 });
