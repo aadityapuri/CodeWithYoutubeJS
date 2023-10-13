bb99207193297f125f534de8627c45ff6a041c77
Aaditya <aadityapuriaaditya@gmail.com>
2023-10-13 23:50:38 +0530

    Continued Promises

--- 09_advance_1/promises.js
+++ 09_advance_1/promises.js
@@ -9,3 +9,20 @@ const promise1 = new Promise(function(resolve, reject){
 promise1.then(function(){
   console.log("Promise consumed!");
 });
+
+new Promise((resolve, reject)=>{
+  setTimeout(() => {
+    console.log("Async task 2");
+    resolve();
+  }, 1000);
+}).then(function(){
+  console.log("Async 2 resolved!");
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve({username: "Aadi", email: "aadityapuriaaditya@gmail.com"})
+  }, 1000);
+}).then((user)=>{
+  console.log(user);
+})
