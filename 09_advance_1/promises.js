a84d79bb19ce0fdfc14074617e7fea5773feb134
Aaditya <aadityapuriaaditya@gmail.com>
2023-10-14 12:55:50 +0530

    Complete Fetch and promises

--- 09_advance_1/promises.js
+++ 09_advance_1/promises.js
@@ -4,7 +4,7 @@ const promise1 = new Promise(function(resolve, reject){
   setTimeout(()=>{
     console.log('Async task is completed');
     resolve();
-  },1000);
+  },800);
 });
 promise1.then(function(){
   console.log("Promise consumed!");
@@ -25,4 +25,67 @@ const promise3 = new Promise((resolve, reject) => {
   }, 1000);
 }).then((user)=>{
   console.log(user);
+});
+
+const promise4 = new Promise(function(resolve, reject) {
+  setTimeout(() => {
+    let error = true;
+    if(!error){
+      resolve({username: "Aadi", pass: "1234"});
+    }
+    else{
+      reject("ERROR: Something went wrong!");
+    }
+  }, 1000);
+});
+promise4.then((user)=>{
+  console.log(user);
+  return user.username;
+}).then((username)=>{
+  console.log(username);
+}).catch(function(error){
+  console.log(error);
+}).finally(()=>{
+  console.log("Promise is resolved or rejected");
 })
+
+
+const promise5 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    let error = true;
+    if(!error){
+      resolve({username: "Js", pass: "789"});
+    }
+    else{
+      reject("ERROR: JS went wrong!");
+    }
+  }, 1000);
+});
+async function consumePromise5(){
+  try {
+    const response = await promise5;
+    console.log(response);
+    
+  } catch (error) {
+    console.log(error);
+  }
+}
+consumePromise5();
+
+// async function getAllUsers(){
+//   try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = await response.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// getAllUsers();
+
+fetch('https://api.github.com/users/aadityapuri')
+.then((response)=>{
+  return response.json();
+})
+.then((data)=>console.log(data))
+.catch((error)=> console.log(error));
